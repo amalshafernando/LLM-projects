967d363882b6af504f6fc9987d51a86ccac0d3c6
Amalsha Fernando <it20055158@my.sliit.lk>
2024-01-31 10:19:55 +0530

    Q & A bot

--- Q and A chat bot/app.py
+++ Q and A chat bot/app.py
@@ -1,28 +1,38 @@
 # Q&A Chatbot
 from langchain.llms import OpenAI
+
 from dotenv import load_dotenv
 
-# load_dotenv()  # take environment variables from .env.
+#load_dotenv()  # take environment variables from .env.
 
 import streamlit as st
 import os
 
-## Function to load OpenAI model and get responses
+
+## Function to load OpenAI model and get respones
+
 def get_openai_response(question):
-    llm = OpenAI(openai_api_key=os.getenv("OPEN_API_KEY"), model_name="text-davinci-003", temperature=0.5)
-    response = llm(question)
+#    llm=OpenAI(model_name="text-davinci-003",temperature=0.5)
+     llm=OpenAI(openai_api_key=os.getenv("OPEN_API_KEY"), model_name="text-davinci-003",temperature=0.5)
+    response=llm(question)
     return response
 
-## initialize our streamlit app
+##initialize our streamlit app
+
 st.set_page_config(page_title="Q&A Demo")
+
 st.header("Langchain Application")
 
-input_text = st.text_input("Input: ", key="input")
-response = get_openai_response(input_text)
+input=st.text_input("Input: ",key="input")
+response=get_openai_response(input)
 
-submit = st.button("Ask the question")
+submit=st.button("Ask the question")
 
-## If the Ask button is clicked
+## If ask button is clicked
+
 if submit:
     st.subheader("The Response is")
     st.write(response)
+
+
+
