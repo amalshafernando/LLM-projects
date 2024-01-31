504ebf6c2df2d8a7e5148975c02b82e59f6c0faa
Amalsha Fernando <it20055158@my.sliit.lk>
2024-01-31 10:16:48 +0530

    Q & A Bot

---
+++ Q and A chat bot/app.py
@@ -0,0 +1,28 @@
+# Q&A Chatbot
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
+
+# load_dotenv()  # take environment variables from .env.
+
+import streamlit as st
+import os
+
+## Function to load OpenAI model and get responses
+def get_openai_response(question):
+    llm = OpenAI(openai_api_key=os.getenv("OPEN_API_KEY"), model_name="text-davinci-003", temperature=0.5)
+    response = llm(question)
+    return response
+
+## initialize our streamlit app
+st.set_page_config(page_title="Q&A Demo")
+st.header("Langchain Application")
+
+input_text = st.text_input("Input: ", key="input")
+response = get_openai_response(input_text)
+
+submit = st.button("Ask the question")
+
+## If the Ask button is clicked
+if submit:
+    st.subheader("The Response is")
+    st.write(response)
